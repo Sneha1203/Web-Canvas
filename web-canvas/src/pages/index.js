2e41764a7c4db96c6f68adaec19f5605dfd1702a
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2024-07-31 01:25:05 +0530

    linked toolbar and menu using reduc toolkit and slices

--- web-canvas/src/pages/index.js
+++ web-canvas/src/pages/index.js
@@ -1,11 +1,13 @@
 import Menu from '../components/Menu';
 import Toolbar from '../components/Toolbar';
+import Board from '../components/Board';
 
 export default function Home() {
   return (
     <>
       <Menu />
       <Toolbar />
+      <Board />
     </>
   )
 }
